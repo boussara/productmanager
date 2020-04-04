c079470e003d87fe71a86b360cac65ece7786031
Mirage <you@example.cominventoory@gmail.com>
2020-04-04 19:30:41 +0100

    create services and repository

---
+++ src/main/java/com/mirage/java/productmanager/Product.java
@@ -0,0 +1,100 @@
+package com.mirage.java.productmanager;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    
+    private long id;
+    private String name;
+    private String brand;
+    private String madein;
+    private Float price;
+
+    protected Product() {
+    }
+
+
+
+    
+
+
+    
+
+    /**
+     * @return long return the id
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType)
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return String return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return String return the madein
+     */
+    public String getMadein() {
+        return madein;
+    }
+
+    /**
+     * @param madein the madein to set
+     */
+    public void setMadein(String madein) {
+        this.madein = madein;
+    }
+
+    /**
+     * @return Float return the price
+     */
+    public Float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+}
---
+++ src/main/java/com/mirage/java/productmanager/ProductService.java
@@ -0,0 +1,39 @@
+package com.mirage.java.productmanager;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repo;
+
+    public List <Product> listAll()
+    {
+
+        return repo.findAll();
+
+    }
+
+    public void save(Product product) {
+
+        repo.save(product);
+        
+        
+    }
+
+    public Product get(long id){
+
+    return repo.findById(id).get();
+
+    }
+
+   public void delet(long id) {
+     
+    repo.deleteById(id);
+    }
+
+}
