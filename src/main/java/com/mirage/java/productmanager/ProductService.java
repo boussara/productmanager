2f0c1a86d30ab5eb15fe7e1e851cc5e58d396271
Mirage <you@example.cominventoory@gmail.com>
2020-04-07 13:58:53 +0100

    It is ok

--- src/main/java/com/mirage/java/productmanager/ProductService.java
+++ src/main/java/com/mirage/java/productmanager/ProductService.java
@@ -2,10 +2,13 @@ package com.mirage.java.productmanager;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ProductService {
 
     @Autowired
@@ -31,7 +34,7 @@ public class ProductService {
 
     }
 
-   public void delet(long id) {
+   public void delete(long id) {
      
     repo.deleteById(id);
     }
